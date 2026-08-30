c836dc2be6f0ef75dcc999f7a3c9b1c92e5cbf9b
Mike <vanamburg.mv@gmail.com>
2026-08-30 18:59:52 -0500

    Initial Commit

---
+++ Hex/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hex
+//
+//  Created by Mike Van Amburg on 8/30/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
